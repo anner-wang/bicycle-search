a02e2a3083874cbe049e4318b9038606af0998b5
anner <741679043@qq.com>
2019-02-23 00:31:31 +0800

    添加批量物体检测

--- ssd/detection-images.py
+++ ssd/detection-images.py
@@ -21,21 +21,9 @@ for name in os.listdir(images_path):
         left_top_y=bounding_boxs[0][i][1].asscalar()
         right_bottom_x=bounding_boxs[0][i][2].asscalar()
         right_bottom_y=bounding_boxs[0][i][3].asscalar()
-        height=right_bottom_y-left_top_y
-        width=right_bottom_x-left_top_x
         if scores[0][i]>0.5:
-            if class_IDS[0][i]==6:
+            if class_IDS[0][i]==1:
                 cv2.rectangle(img,(left_top_x,left_top_y),(right_bottom_x,right_bottom_y),(0,0,255),2)
-                distance=round(focalLength*CAR_WIDTH/(width),2)
-                cv2.putText(img,str(distance)+'cm',(int(left_top_x),int(left_top_y-10)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-	            0.5, (0, 0, 255),1)
-            if class_IDS[0][i]==14:
-                cv2.rectangle(img,(left_top_x,left_top_y),(right_bottom_x,right_bottom_y),(0,255,0),2)
-                distance=round(focalLength*PERSON_WIDTH/(width),2)
-                cv2.putText(img,str(distance)+'cm',(int(left_top_x),int(left_top_y-10)),
-                cv2.FONT_HERSHEY_SIMPLEX,
-	             0.4, (0, 255,0),1)
     if os.path.exists(pro_images_path+name):
         print('error:file already exist')
     else:
